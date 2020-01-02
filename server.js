3a5e51b0ad74201ad92bb7f9216d7f76fe9a061a
Maximilian Evans <156max@gmail.com)>
2020-01-02 18:11:55 -0500

    max

--- server.js
+++ server.js
@@ -6,7 +6,9 @@ const port = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
+  var string = "Hello World From: \n";
+  string = string +"Max \n";
+  res.end(string);
 });
 
 server.listen(port, hostname, () => {
