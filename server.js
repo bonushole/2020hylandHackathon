570d0c509b13bbb48ddc8f49f6b0d33bfc85fd29
Maximilian Evans <156max@gmail.com)>
2020-01-03 18:24:02 -0500

    switched to tabs for qol

--- server.js
+++ server.js
@@ -4,13 +4,19 @@ const hostname = '127.0.0.1';
 const port = 3000;
 
 const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  var string = "Hello World From: \n";
-  string = string +"Max \n";
-  res.end(string);
+	res.statusCode = 200;
+	var q = url.parse(req.url, true);	
+	if(q.pathname=="/upload"){
+		
+	}
+	else{
+		res.setHeader('Content-Type', 'text/plain');
+		var string = "Hello World From: \n";
+		string = string +"Max \n";
+		res.end(string);
+	}
 });
 
 server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+	console.log(`Server running at http://${hostname}:${port}/`);
 });
