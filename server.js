ca9c6541a12705f053f74d6f6b2d47be884c9ec5
Maximilian Evans <156max@gmail.com)>
2020-01-03 18:46:32 -0500

    added upload

--- server.js
+++ server.js
@@ -1,4 +1,7 @@
 const http = require('http');
+var formidable = require('formidable');
+var url = require('url');
+var fs = require('fs');
 
 const hostname = '127.0.0.1';
 const port = 3000;
@@ -7,13 +10,31 @@ const server = http.createServer((req, res) => {
 	res.statusCode = 200;
 	var q = url.parse(req.url, true);	
 	if(q.pathname=="/upload"){
-		
-	}
-	else{
+		var form = new formidable.IncomingForm();
+		form.parse(req, function (err, fields, files) {
+		var oldpath = files.filetoupload.path;
+		var newpath = './videos/' + 		files.filetoupload.name;
+		fs.rename(oldpath, newpath, function (err) {
+			if (err) throw err;
+			res.write('File uploaded and moved!');
+			res.end();
+		});
+	});
+	} else{
+
+
+		res.writeHead(200, {'Content-Type': 'text/html'});
+		res.write('<form action="upload" method="post" enctype="multipart/form-data">');
+		res.write('<input type="file" name="filetoupload"><br>');
+		res.write('<input type="submit">');
+		res.write('</form>');
+		return res.end();
+		/*
 		res.setHeader('Content-Type', 'text/plain');
 		var string = "Hello World From: \n";
 		string = string +"Max \n";
 		res.end(string);
+		*/
 	}
 });
 
