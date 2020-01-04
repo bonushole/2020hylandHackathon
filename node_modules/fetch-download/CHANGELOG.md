98809b0325b2d9edc80b073c19c014bf97955373
Noxid00 <Noxid00@gmail.com>
2020-01-04 13:16:11 -0500

    com

--- node_modules/fetch-download/CHANGELOG.md
+++
@@ -1,4 +0,0 @@
-0.1.1 / 2018-06-07
-==================
-
-  * Support for an options parameter
--- node_modules/node-video-lib/lib/codecs/utils.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-module.exports = {
-    CODEC_AAC: 'aac',
-    CODEC_H264: 'h264',
-    CODEC_H265: 'h265',
-};
